c21ac87813888b0f3724b02b044a1347c26d04fb
Victor Chugunov <vichugunov@gmail.com>
2022-04-04 13:17:35 +0300

    add done console.log

--- module.js
+++ module.js
@@ -2,6 +2,7 @@ const functionUnderTest = async (helper) => {
   helper.transaction(async (transaction) => {
     await helper.doSomething();
     await helper.expectedToBeCalled();
+    console.log("done");
   });
 };
 module.exports = {
